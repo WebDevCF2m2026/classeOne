cf0a7e60bf97bd80a35dde68cfd604f97955701a
WebDevCF2m <gitweb@cf2m.be>
2026-03-31 13:49:13 +0200

    Create homepage's view without get variable

--- public/index.php
+++ public/index.php
@@ -10,10 +10,26 @@
 
 require_once '../config.php';
 
-// test de la constante racine
+/*******************
+ * Routage entre les
+ * diverses vues
+ *******************/
+
+// si non existence de la variable
+// $_GET nommée 'p'
+if(!isset($_GET['p'])){
+
+    // Nous sommes sur l'accueil
+    // chargement de view/homepage.php
+    include ROOT_PATH."/view/homepage.php";
+
+}
+
+/* test de la constante racine
 echo "Racine du projet : ".ROOT_PATH."<br>";
 
 // affichage des pages acceptées
 echo "<pre>";
 print_r(ARRAY_VALID_PAGES);
 echo "</pre>";
+*/
